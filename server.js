af880cc2753cead77baf8729ae115301560c835f
likalika24 <lika.benjusite@tktk.ee>
2023-04-09 14:18:50 +0300

    add search

--- server.js
+++ server.js
@@ -58,6 +58,6 @@ app.post('/search', (req, res) => {
     
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log('Server is running.');
 });
