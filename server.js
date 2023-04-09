adb3c9417ae6c39388170a607f1ac75bc71af19e
likalika24 <lika.benjusite@tktk.ee>
2023-04-09 14:21:37 +0300

    git add

--- server.js
+++ server.js
@@ -58,6 +58,6 @@ app.post('/search', (req, res) => {
     
 });
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running.');
 });
